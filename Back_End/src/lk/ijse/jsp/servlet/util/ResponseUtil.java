9ed20550e1f7d9663b411bc1a718078c3d99c38e
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-07 23:19:48 +0530

    added

--- Back_End/src/lk/ijse/jsp/servlet/util/ResponseUtil.java
+++ Back_End/src/lk/ijse/jsp/servlet/util/ResponseUtil.java
@@ -1,4 +1,12 @@
 package lk.ijse.jsp.servlet.util;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
 public class ResponseUtil {
+    public static JsonObject genJson(String state , String message , JsonArray...date){
+        JsonObjectBuilder object = Json.createObjectBuilder();
+    }
 }
